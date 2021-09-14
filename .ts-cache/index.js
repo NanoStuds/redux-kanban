ba897b3a8f1a50bd5f13ca1d1472cb94bc124198
Hiroki Ito <diavolos1230@gmail.com>
2021-09-14 23:57:17 +0900

    from scratch

--- .ts-cache/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { GlobalStyle } from './GlobalStyle';
-import { App } from './App';
-ReactDOM.render(React.createElement(React.Fragment, null,
-    React.createElement(GlobalStyle, null),
-    React.createElement(App, null)), document.getElementById('app'));
--- test/index.ts
+++
@@ -1,2 +0,0 @@
-import './reducer.test'
-import './util.test'
